c671a5108bc442b6510ae7c1501b317818e849cb
Sergio Acosta <costa@Sergios-MacBook-Air.local>
2018-08-06 10:22:39 +0100

    adding git

---
+++ README.md
@@ -0,0 +1,22 @@
+# Webpack Starter
+
+A webpack starter with separate development and production configurations
+
+Both configurations support:
+
+- loading of CSS, image and font assets,
+- transpilation of JavaScript down to ES5,
+- generation of an HTML index file.
+
+The production build supports
+
+- [minification][minification]
+- [tree shaking][treeshaking].
+
+The development build supports
+
+- [hot module reloading][hmr]
+
+[minification]: https://en.wikipedia.org/wiki/Minification_(programming)
+[treeshaking]: https://developer.mozilla.org/en-US/docs/Glossary/Tree_shaking
+[hmr]: https://webpack.js.org/concepts/hot-module-replacement/
---
+++ webpack.common.js
@@ -0,0 +1,42 @@
+const path = require("path");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const outDir = "public";
+
+module.exports = {
+	entry: {
+		app: "./src/index.js",
+	},
+	output: {
+		filename: "[name].bundle.js",
+		path: path.join(__dirname, outDir),
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: "babel-loader",
+					options: {
+						presets: ["env"],
+					},
+				},
+			},
+			{ test: /\.css$/, use: ["style-loader", "css-loader"] },
+			{ test: /\.(jpg|png|gif|svg|tiff)$/, use: "file-loader" },
+			{
+				test: /\.(woff|woff2|eot|ttf|otf)$/,
+				use: "file-loader",
+			},
+		],
+	},
+	plugins: [
+		new CleanWebpackPlugin([outDir]),
+		new HtmlWebpackPlugin({
+			title: "Webpack Starter",
+			inject: "body",
+		}),
+	],
+};
